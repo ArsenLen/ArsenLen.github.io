c7ad6d15d70fad9d04cf5a4614c9b2439793b02c
ArsenLen <laurie10@mail.ru>
2021-08-28 16:56:58 +0600

    update my files

--- js/langSwitch.js
+++
@@ -1,6 +0,0 @@
-$(".langswitcher a").on("click",function(e){
-    e.preventDefault();
-    $(".langswitcher li").removeClass("active");
-    $("html").attr("lang",$(this).data("lang"));
-    $(this).parent().addClass("active");
-  });
