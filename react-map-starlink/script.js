b941711e6a7a5328a6b4c6020bd514a12920f7c4
ArsenLen <57669561+ArsenLen@users.noreply.github.com>
2022-05-27 21:36:31 +0600

    beta

---
+++ react-map-starlink/script.js
@@ -0,0 +1,29 @@
+var map = L.map('map').setView([41.5, 74.582748], 7);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+fetch("https://api.jsonbin.io/b/6290ec3f449a1f3821f1cb80")
+    .then(res => res.json())
+        .then(data => renderCarMarkers(data.devices));
+
+const myIcon = L.icon({
+    iconUrl: './car-icon.png',
+    iconSize: [15, 15],
+});
+        
+
+const renderCarMarkers = (cars) => {
+    cars.map(car => {
+        console.log(car.position.x)
+        console.log(car.position.y)
+        L.marker([car.position.y, car.position.x], {
+            title : car.alias,
+            alr : car.alias,
+            icon : myIcon
+        }).addTo(map)
+    })
+}
+
